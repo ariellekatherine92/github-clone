667436ec10250221d1d78991049a151091ed0db3
“ariellekatherine92” <ariellekatherine92@gmail.com>
2021-06-01 21:26:41 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
+import Dashboard from "./components/Dashboard"; 
+import Explore from "./components/Explore"; 
+import Repositories from "./components/Repositories"; 
+import Settings from "./components/Settings"; 
+const URL = "https://api.github.com/users/romebell";
+
 
 function App() {
   return (
     <div className="App">
       <h1>Github Clone</h1>
       <img width="350px" height="200px" src="https://img.flaticon.com/icons/png/512/25/25231.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF" alt="" />
+    <Dashboard/>
     </div>
   );
 }
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,21 @@
 import React, { useState, useEffect } from 'react';
 
 const Dashboard = (props) => {
-
+    const gitDashboard = props.dashboard.map(dashboard=>{
+        let location = {
+            pathname:"/dashboard",
+            state: dashboard
+        }
+        return (<Link key={'in_'+dashboard.name}to={location}>{dashboard.name}</Link>)
+    })
+    return(<div className="Dashboard">
+        <h1>Dashboard</h1>
+        <div className="dashboard-wrap">
+        {props.dashboard.length ? Dashboard : <p>...Loading</p>}
+        </div>
+        
+    </div>)
 }
+
 
 export default Dashboard;
